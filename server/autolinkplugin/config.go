7eda184bc25598212ede3403d6ea6befc6c8bbb2
Jason Frerich <jason.frerich@mattermost.com>
2020-03-12 21:42:08 -0500

    initial proposal

--- server/autolinkplugin/config.go
+++ server/autolinkplugin/config.go
@@ -15,6 +15,9 @@ type Config struct {
 	EnableAdminCommand bool
 	EnableOnUpdate     bool
 	Links              []autolink.Autolink
+	EnableVisaCard     bool
+	EnableMasterCard   bool
+	EnableSSN          bool
 }
 
 // OnConfigurationChange is invoked when configuration changes may have been made.
@@ -24,6 +27,8 @@ func (p *Plugin) OnConfigurationChange() error {
 	if err != nil {
 		return err
 	}
+
+	c.Links = p.AddPreConfigLinks(&c)
 
 	for i := range c.Links {
 		err = c.Links[i].Compile()
@@ -78,6 +83,35 @@ func (p *Plugin) SaveLinks(links []autolink.Autolink) error {
 	return nil
 }
 
+// GetPreConFigLinks gets the preconfigured links from the plugin config
+func (p *Plugin) AddPreConfigLinks(c *Config) []autolink.Autolink {
+	link := autolink.Autolink{
+		Name:     "VisaCard",
+		Pattern:  "(?P<VISA>(?P<part1>4\\d{3})[ -]?(?P<part2>\\d{4})[ -]?(?P<part3>\\d{4})[ -]?(?P<LastFour>[0-9]{4}))",
+		Template: "VISA XXXX-XXXX-XXXX-$LastFour",
+		Disabled: !c.EnableVisaCard,
+	}
+	c.Links = append(c.Links, link)
+
+	link = autolink.Autolink{
+		Name:     "MasterCard",
+		Pattern:  "(?P<MasterCard]((?P<part1>5[1-5]\\d{2})[ -]?(?P<part2>\\d{4})[ -]?(?P<part3>\\d{4})[ -]?(?P<LastFour>[0-9]{4}))",
+		Template: "MasterCard XXXX-XXXX-XXXX-$LastFour",
+		Disabled: !c.EnableMasterCard,
+	}
+	c.Links = append(c.Links, link)
+
+	link = autolink.Autolink{
+		Name:     "SSN",
+		Pattern:  "(?P<SSN>(?P<part1>\\d{3})[ -]?(?P<part2>\\d{2})[ -]?(?P<LastFour>[0-9]{4}))",
+		Template: "XXX-XX-$LastFour",
+		Disabled: !c.EnableSSN,
+	}
+	c.Links = append(c.Links, link)
+
+	return c.Links
+}
+
 func (p *Plugin) UpdateConfig(f func(conf *Config)) Config {
 	p.confLock.Lock()
 	defer p.confLock.Unlock()
@@ -97,6 +131,9 @@ func (conf Config) ToConfig() map[string]interface{} {
 		"EnableAdminCommand": conf.EnableAdminCommand,
 		"EnableOnUpdate":     conf.EnableOnUpdate,
 		"Links":              links,
+		"EnableVisaCard":     conf.EnableVisaCard,
+		"EnableMasterCard":   conf.EnableMasterCard,
+		"EnableSSN":          conf.EnableSSN,
 	}
 }
 
