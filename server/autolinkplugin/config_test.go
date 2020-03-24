419ff7703171cc1ecb0ef30d4573761100060635
mo2menelzeiny <mo2menelzeiny@gmail.com>
2020-03-24 22:11:05 +0200

    organized test cases using subtests

--- server/autolinkplugin/config_test.go
+++ server/autolinkplugin/config_test.go
@@ -10,51 +10,54 @@ import (
 	"testing"
 )
 
-func TestOnConfigurationChangeInvalidConfig(t *testing.T) {
-	api := &plugintest.API{}
-	api.On("LoadPluginConfiguration",
-		mock.AnythingOfType("*autolinkplugin.Config")).Return(func(dest interface{}) error {
-		return errors.New("LoadPluginConfiguration Error")
+func TestOnConfigurationChange(t *testing.T) {
+
+	t.Run("Invalid Configuration", func(t *testing.T) {
+		api := &plugintest.API{}
+		api.On("LoadPluginConfiguration",
+			mock.AnythingOfType("*autolinkplugin.Config")).Return(func(dest interface{}) error {
+			return errors.New("LoadPluginConfiguration Error")
+		})
+
+		p := Plugin{}
+		p.SetAPI(api)
+
+		err := p.OnConfigurationChange()
+		assert.Error(t, err)
 	})
 
-	p := Plugin{}
-	p.SetAPI(api)
-
-	err := p.OnConfigurationChange()
-	assert.Error(t, err)
-}
-
-func TestOnConfigurationChangeInvalidLink(t *testing.T) {
-	conf := Config{
-		Links: []autolink.Autolink{
-			autolink.Autolink{
-				Name:     "existing",
-				Pattern:  ")",
-				Template: "otherthing",
+	t.Run("Invalid Autolink", func(t *testing.T) {
+		conf := Config{
+			Links: []autolink.Autolink{
+				autolink.Autolink{
+					Name:     "existing",
+					Pattern:  ")",
+					Template: "otherthing",
+				},
 			},
-		},
-	}
+		}
 
-	api := &plugintest.API{}
-	api.On("LoadPluginConfiguration",
-		mock.AnythingOfType("*autolinkplugin.Config")).Return(func(dest interface{}) error {
-		*dest.(*Config) = conf
-		return nil
+		api := &plugintest.API{}
+		api.On("LoadPluginConfiguration",
+			mock.AnythingOfType("*autolinkplugin.Config")).Return(func(dest interface{}) error {
+			*dest.(*Config) = conf
+			return nil
+		})
+
+		api.On("LogError",
+			mock.AnythingOfType("string"),
+			mock.AnythingOfType("string"),
+			mock.AnythingOfType("Autolink"),
+			mock.AnythingOfType("string"),
+			mock.AnythingOfType("string")).Return(nil)
+
+		api.On("UnregisterCommand", mock.AnythingOfType("string"),
+			mock.AnythingOfType("string")).Return((*model.AppError)(nil))
+
+		p := New()
+		p.SetAPI(api)
+		p.OnConfigurationChange()
+
+		api.AssertNumberOfCalls(t, "LogError", 1)
 	})
-
-	api.On("LogError",
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("Autolink"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("string")).Return(nil)
-
-	api.On("UnregisterCommand", mock.AnythingOfType("string"),
-		mock.AnythingOfType("string")).Return((*model.AppError)(nil))
-
-	p := New()
-	p.SetAPI(api)
-	p.OnConfigurationChange()
-
-	api.AssertNumberOfCalls(t, "LogError", 1)
 }
