8053e0064ef54cd45f9690b678eb5eea038917fe
ByeongsuPark <39683194+ByeongsuPark@users.noreply.github.com>
2020-10-26 18:43:33 +0900

    Fix a typo

--- server/autolinkplugin/config.go
+++ server/autolinkplugin/config.go
@@ -67,7 +67,7 @@ func (p *Plugin) OnConfigurationChange() error {
 
 func getAutoCompleteData() *model.AutocompleteData {
 	autolink := model.NewAutocompleteData("autolink", "[command]",
-		"Available command : add, delete, disable, enable list, set, test")
+		"Available command : add, delete, disable, enable, list, set, test")
 
 	add := model.NewAutocompleteData("add", "",
 		"Add a new link with a given name")
